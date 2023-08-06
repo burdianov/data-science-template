ddc69af57e9505c7b0e359db4542bf044423cfde
Lilian Burdianov <lilian.burdianov@gmail.com>
2023-08-06 20:47:32 +0400

    new feature

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
 
-df2 = pd.DataFrame({"C": [1, 2, 3], "D": [4, 5, 6]})
+# New feature
+
+df2 = pd.DataFrame({"C": [1, 2, 3, 4], "D": [5, 6, 7, 8]})
